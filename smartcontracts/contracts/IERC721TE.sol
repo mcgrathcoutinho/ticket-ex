8fcd36ed36b58c7d05f3fdd71f477d1db430309e
wurdig-mich <harshay.khetan@gmail.com>
2022-11-08 19:13:24 +0530

    init

---
+++ smartcontracts/contracts/IERC721TE.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+
+interface IERC721TE is IERC721, IERC721Receiver, IERC721Metadata {
+    function attend(uint256[] calldata tokenIds) external;
+}
