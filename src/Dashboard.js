a1429ad9dc31612467040caf37372aa503d0cbda
ryan <penn@ryans-mbp.myfiosgateway.com>
2022-11-07 21:14:12 -0500

    add navbar, carousel

---
+++ src/Dashboard.js
@@ -0,0 +1,49 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import EventCarousel from './EventCarousel';
+import Navbar from './Navbar';
+
+const events = [
+    {
+        eventId: 1,
+        name: "ETHCRUNCH 2023",
+        startDate: "2023-04-13 16:00:00PM",
+        endDate: "2023-04-16 12:00:00PM",
+        venue: "The Bunker",
+        location: "Medellin, Colombia",
+        imagePath: "eth"
+    },
+    {
+        eventId: 2,
+        name: "Rubber & Road",
+        startDate: "2023-03-12 16:00:00PM ET",
+        endDate: null,
+        venue: "The Forest Point Fairground",
+        location: "Silverstone, England",
+        imagePath: "race"
+    },
+    {
+        eventId: 3,
+        name: "Ephemira Music Fest",
+        startDate: "2023-07-01 17:00:00PM",
+        endDate: null,
+        venue: "Dock Beach",
+        location: "Budva, Montenegro",
+        imagePath: "concert"
+    }
+]
+
+function Dashboard() {
+  return (
+    <Container>
+        <Navbar/>
+        <EventCarousel events={events}/>
+    </Container>
+  );
+}
+
+export { Dashboard };
