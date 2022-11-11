5b10b53958056a67a9290e27a65479832ef3584a
ryan <penn@ryans-mbp.myfiosgateway.com>
2022-11-11 18:37:17 -0500

    start metadata

--- src/routes/CreateEvent.js
+++ src/routes/CreateEvent.js
@@ -4,94 +4,100 @@ import fleekStorage from "@fleekhq/fleek-storage-js";
 
 
 export default function CreateEvent(){
-    const [name, setName] = useState()
-    const [noOfTickets, setNoOfTickets] = useState()
-    const [description, setDescription] = useState()
+	const [name, setName] = useState()
+	const [noOfTickets, setNoOfTickets] = useState()
+	const [description, setDescription] = useState()
 
-  useEffect(() => {
-    console.log({ name, noOfTickets, description });
-  }, [name, noOfTickets, description]);
+	useEffect(() => {
+		console.log({ name, noOfTickets, description });
+	}, [name, noOfTickets, description]);
 
-  let file;
 
-  const uploadHandler = async (e) => {
-    file = e.target.files[0];
-  };
+	let file;
 
-  const createEventBtnHandler = async (e) => {
-    e.preventDefault();
+	const uploadHandler = async (e) => {
+		file = e.target.files[0];
+	};
 
-    const uploadedFile = await fleekStorage.upload({
-        apiKey: "Oxwo61HtXQs/jvFoPiayTg==",
-        apiSecret: "7nLTggZg9mr35tM9mBv86i34nfxaOca/EeNWb6J4Rt4=",
-        key: file.name,
-        ContentType: "multipart/form-data",
-        data: file,
-        httpUploadProgressCallback: (event) => {
-          console.log(Math.round((event.loaded / event.total) * 100) + "% done");
-        },
-      });
-      console.log(file, uploadedFile);
-    };
+	const createEventBtnHandler = async (e) => {
+		e.preventDefault();
+
+		const uploadedFile = await fleekStorage.upload({
+			apiKey: "Oxwo61HtXQs/jvFoPiayTg==",
+			apiSecret: "7nLTggZg9mr35tM9mBv86i34nfxaOca/EeNWb6J4Rt4=",
+			key: file.name,
+			ContentType: "multipart/form-data",
+			data: file,
+			httpUploadProgressCallback: (event) => {
+				console.log(Math.round((event.loaded / event.total) * 100) + "% done");
+			},
+		});
+		
+		const metadata = {
+			"description": description,
+			"external_url": null,
+			"image": uploadedFile.hash,
+			"name": name,
+			"attributes": []
+		}
+		console.log(file, uploadedFile.hash, metadata)  
+	}; 
 
   return (
-    <div>
-      <h1 id="ticketex">TicketEx</h1>
-      <video autoplay muted loop id="myVideo">
-        <source src="./background-video.mov" type="video/mp4" />
-      </video>
-      <form>
-        <h1 id="create-new-event">Create New Event</h1>
-        <ol class="main-details">
-          <li id="nameofevent">
-            Name of Event
-            <br />
-            <input
-              id="textbox1"
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </li>
-          <br />
-          <li id="ticketquantity">
-            Number of Tickets
-            <br />
-            <input
-              id="textbox2"
-              type="number"
-              value={noOfTickets}
-              onChange={(e) => setNoOfTickets(e.target.value)}
-            />
-          </li>
-          <br />
-          <li id="description">
-            Description
-            <br />
-            <input
-              id="textbox3"
-              type="text"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-            />
-          </li>
-          <li>
-            <p>Event Image</p>
-            {/* <button class="upload-file"><img src="./bg-image-input.webp"/> */}
-            <input
-              id="upload-button-extend"
-              type="file"
-              onChange={uploadHandler}
-            />
-            {/* </button> */}
-          </li>
-        </ol>
-        <br />
-        <br />
-        <button id="mint" onClick={createEventBtnHandler}>
-          Create Event
-        </button>
-      </form>
-    </div>
+	<div>
+		<h1 id="ticketex">TicketEx</h1>
+		<form>
+			<h1 id="create-new-event">Create New Event</h1>
+			<ol class="main-details">
+				<li id="nameofevent">
+				Name of Event
+				<br />
+				<input
+					id="textbox1"
+					type="text"
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+				/>
+				</li>
+				<br />
+				<li id="ticketquantity">
+				Number of Tickets
+				<br />
+				<input
+					id="textbox2"
+					type="number"
+					value={noOfTickets}
+					onChange={(e) => setNoOfTickets(e.target.value)}
+				/>
+				</li>
+				<br />
+				<li id="description">
+					Description
+					<br />
+					<input
+						id="textbox3"
+						type="text"
+						value={description}
+						onChange={(e) => setDescription(e.target.value)}
+					/>
+				</li>
+				<li>
+					<p>Event Image</p>
+					{/* <button class="upload-file"><img src="./bg-image-input.webp"/> */}
+					<input
+						id="upload-button-extend"
+						type="file"
+						onChange={uploadHandler}
+					/>
+					{/* </button> */}
+				</li>
+			</ol>
+			<br />
+			<br />
+			<button id="mint" onClick={createEventBtnHandler}>
+				Create Event
+			</button>
+		</form>
+	</div>
   );
 }
