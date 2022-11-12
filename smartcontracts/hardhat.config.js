78b5cdfe77999519ff14739ff40bfdf39b147b26
wurdig-mich <harshay.khetan@gmail.com>
2022-11-12 21:09:25 +0530

    deploy/verify along with abi

--- smartcontracts/hardhat.config.js
+++ smartcontracts/hardhat.config.js
@@ -1,6 +1,36 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+
+require("dotenv").config();
 module.exports = {
-  solidity: "0.8.13",
+  defaultNetwork: "matic",
+  networks: {
+    hardhat: {},
+    matic: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.POLYGON_SCAN_API_KEY,
+  },
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 2000,
+        details: {
+          yul: true,
+          yulDetails: {
+            stackAllocation: true,
+            optimizerSteps: "dhfoDgvulfnTUtnIf",
+          },
+        },
+      },
+    },
+  },
 };
